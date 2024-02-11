f0c8b7346b0bb2ddc18f9133717461a8216337d7
jayanthsaibhogireddy <jayanthsai8172@gmail.com>
2024-02-11 12:54:28 -0500

    Final Commit

---
+++ server.js
@@ -0,0 +1,37 @@
+// Budget API
+
+const express = require('express');
+const app = express();
+const port = 3000;
+const budgetaa = {
+    myBudget: [
+        {
+            title: 'Eat out',
+            budget: 33
+        },
+        {
+            title: 'Rent',
+            budget: 50
+        },
+        {
+            title: 'Grocery',
+            budget: 90
+        },
+    ]
+};
+app.use("/",express.static("public"));
+
+const budget = require("./data.json");
+console.log(budget)
+
+app.get("/hello", (req,res) => {
+    res.send("Hello Abinaya");
+})
+
+app.get("/budget", (req,res) => {
+    res.json(budget);
+})
+
+app.listen(port, () => {
+    console.log(`API served at http://localhost:${port}`);
+});
